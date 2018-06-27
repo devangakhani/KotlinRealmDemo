7622d9ae53afa9e85272e2bf0682024c7041ab12
Devang <devang.akhani@einfochips.com>
2018-06-27 18:31:37 +0530

    UPDATED: remove redundant library

--- app/src/main/java/com/kotlinrealmdemo/RealmApplication.kt
+++ app/src/main/java/com/kotlinrealmdemo/RealmApplication.kt
@@ -1,19 +1,11 @@
 package com.kotlinrealmdemo
 
 import android.app.Application
-import android.util.Log
 import io.realm.Realm
 
 class RealmApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        try {
-
-            Realm.init(this)
-
-        } catch (e: Exception) {
-            Log.e("DevangTest", "Exception in creating realm object", e)
-        }
-
+        Realm.init(this)
     }
 }
